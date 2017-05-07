e8dae133a4cafc00f0bb254c79f2097008bc9851
Kristen Fanning <kristen.fanning@northwestern.edu>
2017-05-07 11:18:18 -0500

    update files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,11 @@
 let friends = require("./data/friends.js")
 
 module.exports = function(app){
-	
+	app.get("/api/friends", (req, res) =>{
+		response.json(friends)
+	})
+
+	app.post("api/friends", function(req, res){
+		
+	})
 }
