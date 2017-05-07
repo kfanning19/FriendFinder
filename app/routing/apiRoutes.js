852426e7c2c9d8b949a33f003b221091366e6ca9
Kristen Fanning <kristen.fanning@northwestern.edu>
2017-05-07 14:00:21 -0500

    fix bugs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,35 +7,38 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
         // Variable from user
-        let newFriend = req.body.scores;
-     
+        let newFriendArray = req.body
+        let newFriend = newFriendArray.scores;
+
         // Best Match variables
-        let matchName = '';
-        let matchImage = '';
-        let totalDifference = 50; //greatest difference possible
+
+        let bestmatch = 0;
+        let totalDifference = 100; //greatest difference possible
 
         // Loop through friends json
         for (var i = 0; i < friends.length; i++) {
-            
+
 
             // Check for each question
             let diff = 0;
             for (var j = 0; j < newFriend.length; j++) {
                 diff += Math.abs(friends[i].scores[j] - newFriend[j]);
+
+
+                // Lower score = a better match, remove previous match if new difference is lower
+                if (diff < totalDifference) {
+
+                    totalDifference = diff;
+                    bestmatch = i;
+
+                }
+
             }
- 
-            // Lower score = a better match, remove previous match if new difference is lower
-            if (diff < totalDifference) {
-
-                totalDifference = diff;
-                matchName = friends[i].name;
-                matchImage = friends[i].avatar;
-            }
-
-
         }
         // Add new friend
         friends.push(req.body)
+        res.json(friends[bestmatch])
+
     })
 
 }
